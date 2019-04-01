c78d34d4eedef4640df49e6e03282969b752d48a
YIRAN GAO <43918870+YiranGao@users.noreply.github.com>
2019-04-01 20:06:17 +0100

    asddf

---
+++ src/com/undertale/fecade/CommandExecution.java
@@ -0,0 +1,46 @@
+package com.undertale.fecade;
+
+import com.undertale.model.Command;
+import com.undertale.model.UndertaleMap;
+
+public class CommandExecution {
+	private HelpExecution help;
+	private GoExecution go;
+	private ReadExecution read;
+	private SaveExecution save;
+	private NewExecution newGame;
+	private CheckExecution check;
+	private AttackExecution attack;
+	
+	
+	public CommandExecution() {
+		help = new HelpExecution();
+		go = new GoExecution();
+		read = new ReadExecution();
+		save = new SaveExecution();
+		newGame = new NewExecution();
+		check = new CheckExecution();
+		attack = new AttackExecution();
+	}
+	
+	public void execute(Command command, UndertaleMap map) {
+		String firstword = command.getfirstWord();
+		if(firstword.equals("HELP")) {
+			help.excute(command, map);
+		} else if(firstword.equals("GO")) {
+			go.excute(command, map);
+		} else if(firstword.equals("READ")) {
+			read.excute(command, map);
+		} else if(firstword.equals("NEW")) {
+			newGame.excute(command, map);
+		} else if(firstword.equals("SAVE")) {
+			save.excute(command, map);
+		} else if(firstword.equals("CHECK")) {
+			check.excute(command, map);
+		} else if(firstword.equals("TAKE")) {
+			attack.excute(command, map);
+		}
+
+	}
+}
+
