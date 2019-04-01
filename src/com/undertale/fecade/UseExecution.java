413e01dd8f9c8911c3a7d94c718233eaedcaa552
Tianqi <ludwika@yeah.net>
2019-04-01 21:23:57 +0800

    Tianqi_UseExecution

---
+++ src/com/undertale/fecade/UseExecution.java
@@ -0,0 +1,18 @@
+package com.undertale.fecade;
+
+import com.undertale.model.Command;
+import com.undertale.model.Room;
+import com.undertale.model.UndertaleMap;
+
+public class UseExecution implements Fecade {
+	UndertaleMap map;
+
+	@Override
+	public void excute(Command command, UndertaleMap map) {
+		// TODO Auto-generated method stub
+		System.out.println("litianqi");
+	}
+	
+	
+
+}
